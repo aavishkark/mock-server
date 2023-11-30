5781643e74da6e77f6c10155f1adc3a480b07222
aviatalma <aavishkark98@gmail.com>
2023-11-30 17:16:37 +0530

    New commit for new eval

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
+var cors = require('cors')
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
-
+server.use(cors)
 server.listen(port);
